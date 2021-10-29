28e3a5d8e4556cb660a7da07dc37c126c86a3a28
Shaurya Vardhan <gsshaurya@gmail.com>
2021-10-30 01:29:00 +0530

    feat: add header to task board

--- components/TaskBoard/TaskBoard.jsx
+++ components/TaskBoard/TaskBoard.jsx
@@ -1,8 +1,9 @@
 import TaskBoardHeader from "../TaskBoardHeader/TaskBoardHeader";
+import styles from "./style.module.css";
 
 export default function TaskBoard() {
   return (
-    <div>
+    <div className={styles.taskBoard}>
       <TaskBoardHeader />
     </div>
   );
--- components/TaskBoardHeader/TaskBoardHeader.jsx
+++ components/TaskBoardHeader/TaskBoardHeader.jsx
@@ -1,5 +1,37 @@
+import { MdDeveloperBoard, MdKeyboardArrowDown } from "react-icons/md";
+import { BiSearchAlt2 } from "react-icons/bi";
+import { BsThreeDots } from "react-icons/bs";
 import styles from "./style.module.css";
 
 export default function TaskBoardHeader() {
-  return <div className={styles.taskBoardHeader}>Task Board Header</div>;
+  return (
+    <div className={styles.taskBoardHeader}>
+      <div className={styles.leftHeader}>
+        <div>
+          <MdDeveloperBoard className={styles.icon} />
+          By Status <MdKeyboardArrowDown className={styles.icon} />
+        </div>
+      </div>
+      <div>
+        <div>Properties</div>
+        <div>Group</div>
+        <div>Sub group</div>
+        <div>Filter</div>
+        <div>Sort</div>
+        <div>
+          <BiSearchAlt2 className={styles.icon} />
+          Search
+        </div>
+        <div>
+          <BsThreeDots className={styles.icon} />
+        </div>
+        <div>
+          <button className={styles.headerButton}>New</button>
+          <button className={styles.headerButton}>
+            <MdKeyboardArrowDown className={styles.icon} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
